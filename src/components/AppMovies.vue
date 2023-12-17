a9f481f2294a1cb7f3ae7a5a3c57bb789a88cfb7
WebGCDev <gaetano.chi@gmail.com>
2023-12-17 18:47:56 +0100

    add: Rating converted into stars & Loading Vote 0 Stars

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -28,8 +28,15 @@ export default {
         <div class="text">
             <div class="title">Titolo: {{ dataMovie.title }}</div>
             <div class="original">Titolo originale: {{ dataMovie.original_title }}</div>
-            <div class="language">{{ dataMovie.original_language }}</div>
-            <div class="vote">Voto: {{ convertVote(dataMovie.vote_average) }}</div>
+            <div class="language">Lingua originale: {{ dataMovie.original_language }}</div>
+            <span>Voto:</span>
+            <font-awesome-icon v-for="star in convertVote(dataMovie.vote_average)" :key="star" :icon="['fas', 'star']"
+                class="star" />
+            <template v-for="star in 5 - convertVote(dataMovie.vote_average)">
+                <font-awesome-icon :key="star" :icon="['far', 'star']" v-if="convertVote(dataMovie.vote_average) < 5"
+                    class="star" />
+            </template>
+
         </div>
     </div>
 </template>
@@ -37,5 +44,9 @@ export default {
 <style lang="scss" scoped>
 .movie {
     display: flex;
+}
+
+.star {
+    color: rgb(252, 0, 0);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,11 +9,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 /* + icons to the library */
 library.add(fas)
-
+library.add(far)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
